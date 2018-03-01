9594e6ccbc1017f3f4106a438b749110df9c4d5f
JTorrus <jtorrusgalindo@gmail.com>
2018-03-01 17:28:19 +0100

    Scene for adding a new item created, ViewController renamed to AddToDo

--- ToDoListLSG/AddToDoViewController.swift
+++ ToDoListLSG/AddToDoViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AddToDoViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var newItemTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
---
+++ ToDoListLSG/ToDoListTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ToDoListTableViewController.swift
+//  ToDoListLSG
+//
+//  Created by Alumne on 1/3/18.
+//  Copyright © 2018 Alumne. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UITableViewController {
+    var toDoItemManager: ToDoItemManager = ToDoItemManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItemManager.items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+        
+        let item: ToDoItem = toDoItemManager.items[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        cell.detailTextLabel?.text = dateFormatter.string(from: item.creationDate)
+        
+        if item.completed {
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.none
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDoItemManager.deleteItem(index: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        toDoItemManager.markItemAsCompleted(itemIndex: indexPath.row)
+        tableView.reloadData()
+    }
+    
+    
+}
